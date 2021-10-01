fd6653e0a89c618f85c49ba4bf38b3bb4be09edc
Paolo Forti <fortipaolo96@gmail.com>
2021-10-01 21:25:56 +0200

    added player grid movement and animations

--- components/characters/player/Player.gd
+++ components/characters/player/Player.gd
@@ -3,15 +3,23 @@ extends CharacterBody2D
 @export_range(8, 64, 8) var tile_size : int = 16
 @export var movement_velocity : int = 8
 
+@onready var animation_tree = $AnimationTree
+
 var input_direction : Vector2
+var look_direction : Vector2
 var new_position : Vector2
 
 var snap_vector = Vector2.ZERO
 
+enum state {
+	LOOK,
+	WALK,
+	RUN
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	snap_vector = Vector2(tile_size, tile_size)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,6 +31,13 @@ func _process(delta):
 
 func process_input(delta):
 	input_direction = get_input()
+	if input_direction == Vector2.ZERO:
+		animation_tree.set("parameters/State/current", state.LOOK)
+	else:
+		look_direction = input_direction
+		animation_tree.set("parameters/State/current", state.WALK)
+	animation_tree.set("parameters/Look/blend_position", look_direction)
+	animation_tree.set("parameters/Walk/blend_position", look_direction)
 	new_position = position + input_direction * tile_size
 
 func get_input():
